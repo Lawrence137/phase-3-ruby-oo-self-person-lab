805b7d24ffd2bc1a3ec6ff53c423888dfc63af78
Lawrence137 <lawrencekimani18@gmail.com>
2023-02-21 13:44:18 +0300

    11 tests remaining

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,52 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene 
+    end
+
+    def happiness=(new_value)
+        @happiness = new_value.clamp(0, 10)
+      end
+    
+      def hygiene=(new_value)
+        @hygiene = new_value.clamp(0, 10)
+      end
+    
+      def happy?
+        @happiness > 7
+      end
+    
+      def clean?
+        @hygiene > 7
+      end
+    
+      def get_paid(salary_amount)
+        @bank_account += salary_amount
+        "all about the benjamins"
+      end
+    
+      def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+      end
+
+      def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+      end
+
+      def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+
+      end
+
+   
+end
